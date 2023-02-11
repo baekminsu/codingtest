235060678c33fd048681dd857e286293846d08d6
baekminsu <chodinggame@naver.com>
2023-02-12 02:03:58 +0900

    15596

---
+++ 15596.py
@@ -0,0 +1,12 @@
+def solve(a: list):
+    return sum(a)
+
+
+"""n = int(input())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+
+#arr = list(map(int,input().split()))
+
+print(solve(arr))"""

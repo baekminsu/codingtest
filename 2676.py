05d0c00e7a3f1ce51704c8338ac951f0087533fb
baekminsu <chodinggame@naver.com>
2023-02-14 23:42:28 +0900

    2676

---
+++ 2676.py
@@ -0,0 +1,11 @@
+T = int(input())
+for i in range(T): #입력횟수
+    arr = input() #리스트만들고
+    for j in range(2,len(arr)):
+        for k in range(int(arr[0])):
+            print("%c" %(arr[j]),end ="")
+    print("") 
+
+
+
+    

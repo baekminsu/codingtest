87019593431e8e1ec9a8ba5a4b8b46da5feb23d0
baekminsu <chodinggame@naver.com>
2023-06-19 23:32:34 +0900

    28235D1

---
+++ 28235.py
@@ -0,0 +1,10 @@
+S = input()
+
+if S[0] == 'S':
+    print("HIGHSCHOOL")
+elif (S[0] == 'C') :
+    print("MASTER")
+elif (S[0] == '2') :
+    print("0611")
+elif (S[0] == 'A') :
+    print("CONTEST")

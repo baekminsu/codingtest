5b92b5c2ca931b1b202857eb09f41612a88bb10d
baekminsu <chodinggame@naver.com>
2023-07-03 06:11:20 +0900

    1859D4

--- swea/1859.c
+++ swea/1859.c
@@ -1,40 +1,52 @@
 #include <stdio.h>
 #include <string.h>
-#include <string.h>
+#include <stdlib.h>
 
 #define _crt_secure_no_warnings
+#pragma warning(disable:4996)  // swea 에서 이거쓰면 오류남 c++ 지원하고 c언어 선택란없음
 
 int main() {
 
-	int T,N;
-	int i, j;
+	int T, N;
+	int input;
+	int max;
+	long long sum = 0;
 	scanf("%d", &T);
-	for (i = 0; i < T; i++) {
+
+	for (int i = 0; i < T; i++) {
+		sum = 0;
 		scanf("%d", &N);
-		int *arr= (int*)malloc(sizeof(int) * N);
-		for (j = 0; j < N; j++) { // 배열숫자넣는부분
-			int input;
+		int* arr = (int*)malloc(sizeof(int) * N);
+
+		for (int j = 0; j < N; j++) {
 			scanf("%d", &input);
 			arr[j] = input;
 		}
-		int max = arr[0];
-		int sum = 0;
-		sum += arr[0];
-		for (j = 0; j < N; j++) {
-			if (max < arr[j]) {
-				
-			}
+		max = arr[N - 1];
+		for (int j = N - 2; j >= 0; j--) {
 			if (max > arr[j]) {
-				sum += arr[j];
+				sum += max - arr[j];
+			}
+			else if (max < arr[j]) {
+				max = arr[j];
+				if (j != 0) {
+					continue;
+				}
+				else if (j == 0) {
+					break;
+				}
 			}
 		}
 
-
-
+		printf("#%d %ld\n", i + 1, sum);
 		free(arr);
 	}
 
-
-
 	return 0;
 }
+
+/*
+1
+10
+1 2 3 4 2 6 1 1 2 1
+*/

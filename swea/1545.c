53634621ba24d5a4f26bc6e0925c60e6581d8438
baekminsu <chodinggame@naver.com>
2023-07-08 21:43:05 +0900

    1545D1

---
+++ swea/1545.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define _crt_secure_no_warnings
+#pragma warning(disable:4996)  // swea 에서 이거쓰면 오류남 c++ 지원하고 c언어 선택란없음
+
+int main() {
+
+	int N;
+	scanf("%d ", &N);
+
+	for (int i = N; i >= 0; i--) {
+		printf("%d ", i);
+	}
+
+
+
+	return 0;
+}

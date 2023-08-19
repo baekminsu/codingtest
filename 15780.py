978d6e5ebe5fb005c3d6b70ed186f71475c9774b
baekminsu <chodinggame@naver.com>
2023-08-20 00:19:50 +0900

    d1

--- 15780.py
+++ 15780.py
@@ -1,7 +1,12 @@
 N,K = map(int,input().split())
 multi = list(map(int,input().split()))    
-multi = sum(multi)
-if multi >= N :
+sum = 0
+for i in multi:
+    if i % 2 == 0:
+        sum += i //2
+    elif i % 2 == 1:
+        sum += (i // 2) +1
+if sum < N:
+    print("NO")
+elif sum >= N:
     print("YES")
-else:
-    print("No")

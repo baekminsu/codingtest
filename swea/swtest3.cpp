9cd2a09135f45dcd0ec6f1ecaf5a175882b084a5
baekminsu <110341040+baekminsu@users.noreply.github.com>
2024-01-07 23:50:46 +0900

    Update swtest3.cpp
    240107

--- swea/swtest3.cpp
+++ swea/swtest3.cpp
@@ -6,6 +6,7 @@ int main() {
 	int n;
 	int mid;
 	int result;
+	char feature;
 	cin >> T;
 
 	for (int i = 0; i < T; i++) {
@@ -13,20 +14,45 @@ int main() {
 		int abc[3] = { 0, };
 		int check[3] = { 0, };
 		result = 0;
-		for (int j = 0; j < 3; j++) {
-			cin >> abc[j];
+		for (int k = 0; k < n; k++) {
+			for (int j = 0; j < 3; j++) {
+				cin >> abc[j];
+			}
+
+			mid = abc[0] + abc[1];
+			feature = 'a';
+			if (mid >= (abc[0] + abc[2])) {
+				mid = abc[0] + abc[2];
+				if (mid == (abc[0] + abc[2]) && check[2] != 1 && check[1] != 1) {
+					feature = 'b';
+				}
+			}
+			if (mid >= (abc[1] + abc[2])) {
+				mid = abc[1] + abc[2];
+				if (mid == (abc[0] + abc[2]) && check[2] != 1 && check[0] != 1) {
+					feature = 'c';
+				}
+			}
+
+			if (feature == 'a') {
+				check[2] = 1;
+			}
+			else if (feature == 'b') {
+				check[1] = 1;
+			}
+			else if (feature == 'c') {
+				check[0] = 1;
+			}
+			result += mid;
 		}
 
-		mid = abc[0] + abc[1];
-		if (mid > (abc[0] + abc[2])) {
-			mid = abc[0] + abc[2];
-		}
-		if (mid > (abc[1] + abc[2])) {
-			mid = abc[1] + abc[2];
-		}
-		result += mid;
 
-
+		if (check[0] == 1 && check[1] == 1 && check[2] == 1) {
+			cout << "#" << i + 1 << " " << result << "\n";
+		}
+		else {
+			cout << "#" << i + 1 << " -1" << "\n";
+		}
 	}
 	
 

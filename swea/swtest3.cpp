a9807cfad2cbcef0a430b2aeb72a9419e4dfaa6c
baekminsu <110341040+baekminsu@users.noreply.github.com>
2024-01-08 19:17:01 +0900

    Update swtest3.cpp
    Read the question carefully

--- swea/swtest3.cpp
+++ swea/swtest3.cpp
@@ -3,58 +3,45 @@ using namespace std;
 int main() {
 
 	int T;
-	int n;
-	int mid;
-	int result;
-	char feature;
 	cin >> T;
 
 	for (int i = 0; i < T; i++) {
+		int n;
 		cin >> n;
-		int abc[3] = { 0, };
-		int check[3] = { 0, };
-		result = 0;
-		for (int k = 0; k < n; k++) {
-			for (int j = 0; j < 3; j++) {
-				cin >> abc[j];
-			}
+		int result = 0;
+		int capacity[3];
+		int mincapacity[50][3];
+		int min = 0;
+		for (int j = 0; j < n; j++) {
+			int capacity[3] = {0,};
 
-			mid = abc[0] + abc[1];
-			feature = 'a';
-			if (mid >= (abc[0] + abc[2])) {
-				mid = abc[0] + abc[2];
-				if (mid == (abc[0] + abc[2]) && check[2] != 1 && check[1] != 1) {
-					feature = 'b';
-				}
+			for (int k = 0; k < 3; k++) {
+				cin >> capacity[k];
 			}
-			if (mid >= (abc[1] + abc[2])) {
-				mid = abc[1] + abc[2];
-				if (mid == (abc[0] + abc[2]) && check[2] != 1 && check[0] != 1) {
-					feature = 'c';
-				}
+			if (n < 3) {
+				break;
 			}
+			else {
+				mincapacity[j][0] = capacity[0] + capacity[0];
+				mincapacity[j][1] = capacity[0] + capacity[1];
+				mincapacity[j][2] = capacity[1] + capacity[2];
+			}
+		}
+		if (n < 3) {
+			cout << "#" << i + 1 << " " << -1;
+			continue;
+		}
+		
+		for (int j = 0; j < n; j++) {
+			min = mincapacity[j][0];
+			for (int k = 0; k < 3; k++) {
+				
 
-			if (feature == 'a') {
-				check[2] = 1;
 			}
-			else if (feature == 'b') {
-				check[1] = 1;
-			}
-			else if (feature == 'c') {
-				check[0] = 1;
-			}
-			result += mid;
 		}
 
 
-		if (check[0] == 1 && check[1] == 1 && check[2] == 1) {
-			cout << "#" << i + 1 << " " << result << "\n";
-		}
-		else {
-			cout << "#" << i + 1 << " -1" << "\n";
-		}
 	}
-	
 
 
 	return 0;

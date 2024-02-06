d1ac6c16d8c581c455e03b6c57c1f57b258d343b
baekminsu <chodinggame@naver.com>
2024-02-06 19:15:14 +0900

    programmersfruit...

--- programmers/fruitseller.py
+++ programmers/fruitseller.py
@@ -1,22 +1,8 @@
 def solution(k, m, score):
     answer = 0
-    temp =[]
-    temp2 = []
-    while(len(score) >= m):
-        temp.append(max(score))
-        score.remove(max(score))
-        if len(temp) == m:
-            temp2.append(temp)
-            temp.clear()
-    for i in temp2:
-        if temp2.count(i) == 0:
-            answer += m * min(i)
-        elif temp2.count(i) > 0:
-            save = i
-            savecnt = temp2.count(i)
-            for j in range(savecnt):
-                
-
-
-            
+    score.sort()
+    print(score)
+    for i in range(len(score)-m,-1,-m):
+        if i <= (len(score)-m):
+            answer += m *score[i]       
     return answer
